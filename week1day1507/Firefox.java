1c3492efb74b9d55cd349d046da5db439c4502ad
GeethaKaruppaiah <geethagrasim05@gmail.com>
2024-07-20 20:00:29 +0530

    Add files via upload

---
+++ week1day1507/Firefox.java
@@ -0,0 +1,18 @@
+package week1day1507;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean  isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println(browserVersion+"\n"+ browserName+"\n"+releaseYear+"\n"+isVisible+"\n"+browserLogo);
+	
+
+	}
+
+}
